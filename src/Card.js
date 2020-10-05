4dd20b3af2f0c91bd4a8d848f2bc8362567719c1
mdflynn <mdflynn34@outlook.com>
2020-10-05 13:00:38 -0600

    Add class properties

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,10 @@
 class Card {
-  
+  constructor(id, question, answers, solution) {
+    this.id = id;
+    this.question = question;
+    this.answers = answers;
+    this.solution = solution;
+  }
 }
 
 module.exports = Card;
