550c6e813da2f3f85ad5eeb0e0f28c8e5658b4ae
mdflynn <mdflynn34@outlook.com>
2020-10-05 13:40:44 -0600

    Add return guess and card methods for Turn class

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -26,4 +26,17 @@ describe('Turn', function() {
     const turn = new Turn('object', card);
     expect(turn.currentCard).to.equal(card);
   })
+
+  it.skip('should return the guess', function() {
+    const turn = new Turn('object', card);
+    const userGuess = turn.returnGuess();
+    expect(userGuess).to.equal('object');
+  });
+
+  it.skip('should return the current flash card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('object', card);
+    var cardInPlay = turn.returnCard();
+    expect(cardInPlay).to.equal(card);
+  });
 });
