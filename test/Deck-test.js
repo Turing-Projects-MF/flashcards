eb918577c8b2f9be78b24623c4aa236aaac7fd8b
mdflynn <mdflynn34@outlook.com>
2020-10-05 16:24:46 -0600

    Initial commit

---
+++ test/Deck-test.js
@@ -0,0 +1,34 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', function() {
+
+  it.skip('should be a function', function() {
+    expect(Deck).to.be.a('function');
+  });
+
+  it.skip('should be an instance of Deck', function() {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  });
+
+  it.skip('should start with array of Card objects', function() {
+    const card1 = new Card(1,'What primitave data type returns a true or false?', ['number', 'string', 'boolean'], 'boolean');
+    const card2 = new Card(2, 'Is Javascript dynamically or statically typed?', ['dynamically', 'statically'], 'dynamically');
+    const card3 = new Card(3, 'Can you use for loops in Mod 2?', ['yes', 'no'], 'no');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.cardDeck).to.deep.equal(deck);
+  });
+
+  it.skip('should count how many cards are in a deck', function() {
+    const card1 = new Card(1,'What primitave data type returns a true or false?', ['number', 'string', 'boolean'], 'boolean');
+    const card2 = new Card(2, 'Is Javascript dynamically or statically typed?', ['dynamically', 'statically'], 'dynamically');
+    const card3 = new Card(3, 'Can you use for loops in Mod 2?', ['yes', 'no'], 'no');
+    const deck = new Deck([card1, card2, card3]);
+    const deckCount = deck.countCards();
+    expect(deckCount).to.equal(deck.length);
+  });
+});
