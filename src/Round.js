4275f8f08d361dee2958e6bd0546ffba7f9dec9b
mdflynn <mdflynn34@outlook.com>
2020-10-06 16:00:12 -0600

    Add calculatePercentCorrect and endRound methods

--- src/Round.js
+++ src/Round.js
@@ -9,8 +9,35 @@ class Round {
   }
   takeTurn(guess) {
     this.turns++;
+    // this.checkGuess(guess);
+    // this.updateCardDeck();
     this.deck.push(this.deck.shift());
+    const endOfDeck = this.deck[this.deck.length - 1];
+    if (endOfDeck.correctAnswer === guess) {
+      return 'correct!';
+    } else {
+      this.incorrectGuesses.push(endOfDeck.id)
+      return 'incorrect!';
+    }
   }
+  calculatePercentCorrect() {
+    return Math.round((this.incorrectGuesses.length / this.turns) * 100);
+  }
+  endRound() {
+    const percentCorrect = this.calculatePercentCorrect();
+    return `**Round over!** You answered ${percentCorrect}% of the questions correctly!`;
+  }
+  // updateCardDeck() {
+  //   this.deck.push(this.deck.shift());
+  // }
+  // checkGuess(guess) {
+  //   if (this.deck[0].correctAnswer === guess) {
+  //     console.log('hi');
+  //     return 'correct!';
+  //   } else {
+  //     return 'incorrect!';
+  //   }
+  // }
 }
 
 
