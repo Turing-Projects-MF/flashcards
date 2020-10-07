4aa74e1da0985899e91b838e318864e108eb8601
mdflynn <mdflynn34@outlook.com>
2020-10-06 19:19:52 -0600

    Refactor takeTurn to use Turn class methods

--- src/Round.js
+++ src/Round.js
@@ -1,3 +1,5 @@
+const Turn = require('./Turn.js');
+
 class Round {
   constructor(roundDetails) {
     this.deck = roundDetails.cardDeck;
@@ -8,20 +10,24 @@ class Round {
     return this.deck[0];
   }
   takeTurn(guess) {
+    const currentCard = this.returnCurrentCard();
+    //how to write this test for Turn?
+    const turn = new Turn(guess, currentCard);
+    const result = turn.giveFeedback();
+
     this.turns++;
+    this.checkGuess(guess);
     this.updateCardDeck();
-    return this.checkGuess(guess);
+
+    return result;
+
   }
   updateCardDeck() {
     this.deck.push(this.deck.shift());
   }
   checkGuess(guess) {
-    const endOfDeck = this.deck[this.deck.length - 1];
-    if (endOfDeck.correctAnswer === guess) {
-      return 'correct!';
-    } else {
-      this.incorrectGuesses.push(endOfDeck.id)
-      return 'incorrect!';
+    if (guess !== this.deck[0].correctAnswer) {
+      this.incorrectGuesses.push(this.deck[0].id)
     }
   }
   calculatePercentCorrect() {
@@ -32,6 +38,5 @@ class Round {
     return `**Round over!** You answered ${percentCorrect}% of the questions correctly!`;
   }
 }
-
 
 module.exports = Round;
